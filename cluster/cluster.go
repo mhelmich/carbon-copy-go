018e26f1640b4298f410f86586952d157d8b7491
Marco <marco.helmich@live.com>
2018-01-30 09:17:11 -0800

    - fix cluster compile errors :)

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -2,13 +2,15 @@ package cluster
 
 import (
 	"github.com/coreos/etcd/client"
+	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 type clusterImpl struct {
 	etcdClient client.Client
 }
 
-func createNewCluster() clusterImpl {
+func createNewCluster() *clusterImpl {
 	cfg := client.Config{
 		Endpoints: []string{"http://127.0.0.1:2379"},
 		Transport: client.DefaultTransport,
