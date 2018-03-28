06cd65ba2e9b016271180696933b0be261402547
Marco <marco.helmich@live.com>
2018-03-27 18:38:20 -0700

    - ploished the interface a little bit - added a bunch of comments

--- cluster/interface.go
+++ cluster/interface.go
@@ -26,34 +26,56 @@ type NodeConnectionInfo struct {
 }
 
 type ClusterConfig struct {
-	RaftPort         int
-	RaftStoreDir     string
-	Peers            []string
-	hostname         string
-	RaftServicePort  int
-	SerfPort         int
-	GridPort         int
+	// The port on which raft communicates.
+	RaftPort int
+	// The directory under which raft will write its persistent metadata.
+	RaftStoreDir string
+	// An array of string addresses indicating where the serf service runs.
+	// One other peers is enough to bootstrap this member. However, multiple are
+	// advisable for fault tolerance.
+	Peers []string
+	// Will be set internally.
+	hostname string
+	// The port on which the service operates.
+	RaftServicePort int
+	// The port on which serf operates. This address is supposed to be given
+	// as peer information to bootstrap a new member.
+	SerfPort int
+	// Port on which the Grid service operates.
+	GridPort int
+	// Path under which serf writes its metadata.
 	SerfSnapshotPath string
-	longMemberId     string
-	raftNotifyCh     chan bool
-	logger           *log.Entry
-	NumRaftVoters    int
-	isDevMode        bool
+	// Will be set internally.
+	longMemberId string
+	// Will be set internally.
+	raftNotifyCh chan bool
+	// Will be set internally.
+	logger *log.Entry
+	// The number of raft voters that a cluster should have.
+	// All other members will be added as Nonvoters.
+	NumRaftVoters int
+	// Will be set internally.
+	isDevMode bool
 }
 
+// This is what a cluster looks like to the rest of the world.
 type Cluster interface {
+	// This method will return this members short id.
+	// This id is guaranteed to be unique in the cluster.
 	GetMyShortMemberId() int
 	GetNodeConnectionInfoUpdates() (<-chan []*NodeConnectionInfo, error)
+	// Closes this cluster.
 	Close() error
 }
 
+// Creates a new cluster from the given config.
 func NewCluster(config ClusterConfig) (Cluster, error) {
 	return createNewCluster(config)
 }
 
 type consensusStore interface {
 	get(key string) ([]byte, error)
-	set(key string, value []byte) error
-	delete(key string) error
+	set(key string, value []byte) (bool, error)
+	delete(key string) (bool, error)
 	close() error
 }
